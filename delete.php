b6d5be859ca728f138bd8417e1b6769f038c3a6c
Nicolas-Lalo <eduardo.17070011@itsmotul.edu.mx>
2021-05-29 18:47:42 -0500

    Modificacion de referencia de la hoja de estilos

--- delete.php
+++ delete.php
@@ -26,7 +26,7 @@ include "conn.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <link rel="stylesheet" href="css/mystyles.css">
     <title>Perfil de usuario</title>
 </head>
 <body>
--- update.php
+++ update.php
@@ -58,7 +58,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <link rel="stylesheet" href="css/mystyles.css">
     <title>tabla de proyectos</title>
 </head>
 <body>
