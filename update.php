41d052a42d470da6613f1ccab63455d296d75e1d
Nicolas-Lalo <eduardo.17070011@itsmotul.edu.mx>
2021-05-31 10:50:02 -0500

    Actualizaciond del navbar

--- update.php
+++ update.php
@@ -63,42 +63,45 @@
     <title>tabla de proyectos</title>
 </head>
 <body>
-   <div>
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-                </a>
-
-                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                </a>
-            </div>
-
-            <div id="navbarBasicExample" class="navbar-menu">
-                <div class="navbar-start">
-                    <a class="navbar-item">
-                        Home
+<div>
+        <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="index.php">
+                    <img src="img/logo.png" width="112" height="28" alt="logo">
                     </a>
 
-                    <a class="navbar-item">
-                        Documentation
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
                     </a>
                 </div>
 
-                <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                    <a class="button is-light">
-                        Regístrarse
-                    </a>
-                    <a class="button is-primary">
-                        <strong>Iniciar Sesión</strong>
-                    </a>
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+                        <a class="navbar-item">
+                            Home
+                        </a>
+                        <a class="navbar-item">
+                            Documentation
+                        </a>
+                        <a class="navbar-item">
+                            About Us
+                        </a>
                     </div>
-                </div>
+
+                    <div class="navbar-end">
+                        <a class="navbar-item">
+                            About Us
+                        </a>
+                        <div class="navbar-item">
+                            <div class="buttons">
+                            <a class="button  is-rounded is-outlined is-white">
+                            Log in
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
